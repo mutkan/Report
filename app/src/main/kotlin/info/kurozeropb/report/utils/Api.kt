425906a59d1701ccc45feb1eb18b95a0cd6591b0
KurozeroPB <root@kurozeropb.info>
2019-05-21 12:15:53 +0200

    bump version

--- app/src/main/kotlin/info/kurozeropb/report/utils/Api.kt
+++ app/src/main/kotlin/info/kurozeropb/report/utils/Api.kt
@@ -5,7 +5,7 @@ import info.kurozeropb.report.structures.User
 
 object Api {
     const val baseUrl = "https://reportapp-api.herokuapp.com/v1"
-    const val version = "0.0.11"
+    const val version = "0.0.12"
     const val userAgent = "Report/v$version (https://github.com/reportapp/report)"
 
     var token: String? = null
