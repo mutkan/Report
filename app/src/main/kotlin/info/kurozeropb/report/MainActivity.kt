3ded75824dbeb263599700b5037032d45fe6a952
KurozeroPB <root@kurozeropb.info>
2019-06-21 09:43:01 +0200

    Remove unused import

--- app/src/main/kotlin/info/kurozeropb/report/MainActivity.kt
+++ app/src/main/kotlin/info/kurozeropb/report/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Point
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
